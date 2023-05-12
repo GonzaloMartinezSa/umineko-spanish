35e9da7da8a7c2c5a10ae076a11ffc7373ff2eb8
GonzaloMartinezSa <gmartnezsarmiento@frba.utn.edu.ar>
2023-05-12 13:14:01 -0300

    primera aproximacion

--- src/app.js
+++ src/app.js
@@ -5,8 +5,44 @@
 */
 
 const translate = require('google-translate-api-x');
+const fs = require('fs');
+const readline = require('readline');
+const pth = require('path');
 
-translate('Ik spreek Engels', {to: 'en'}).then(res => {
+const filePathSource = pth.join(__dirname, 'files-english', 'ep1', 'en', 'umi1_op.txt');
+const filePathOutput = pth.join(__dirname, 'files-spanish', 'ep1', 'es', 'umi1_op.txt');
+
+let data = '';
+
+async function traducir(data) {
+  const res = await translate(data, {to: 'es'})
   console.log(res.text); //=> I speak English
-  console.log(res.from.language.iso);  //=> nl
+  //console.log(res.from.language.iso);  //=> nl
+  fs.appendFileSync(filePathOutput, '\n' + res.text);
+}
+
+const fileStream = fs.createReadStream(filePathSource);
+
+const rl = readline.createInterface({
+  input: fileStream,
+  crlfDelay: Infinity
 });
+
+let lineNumber = 0;
+
+rl.on('line', async (line) => {
+  try {
+    await traducir(line);
+    lineNumber++;
+  } catch (err) {
+    console.log(`Llegue hasta: Line ${lineNumber}: ${line}`);
+  }
+});
+
+// try {
+//   data = fs.readFileSync(filePathSource, 'utf8');
+//   console.log(data);
+// } catch (err) {
+//   console.error(err);
+// }
+
