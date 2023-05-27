016f57bf7a95b3100c63fb59383c8eeb0be9a9a4
GonzaloMartinezSa <gmartnezsarmiento@frba.utn.edu.ar>
2023-05-26 22:35:56 -0300

    umi1_op 1era parte + codigo deprecado

--- src/app.js
+++ src/app.js
@@ -12,37 +12,61 @@ const pth = require('path');
 const filePathSource = pth.join(__dirname, 'files-english', 'ep1', 'en', 'umi1_op.txt');
 const filePathOutput = pth.join(__dirname, 'files-spanish', 'ep1', 'es', 'umi1_op.txt');
 
-let data = '';
-
-async function traducir(data) {
-  const res = await translate(data, {to: 'es'})
-  console.log(res.text); //=> I speak English
-  //console.log(res.from.language.iso);  //=> nl
-  fs.appendFileSync(filePathOutput, '\n' + res.text);
-}
-
 const fileStream = fs.createReadStream(filePathSource);
-
 const rl = readline.createInterface({
   input: fileStream,
   crlfDelay: Infinity
 });
 
-let lineNumber = 0;
+// async function traducir(data) {
+//   const res = await translate(data, {to: 'es'})
+//   console.log(res.text); //=> I speak English
+//   fs.appendFileSync(filePathOutput, '\n' + res.text);
+// }
+
+// let lineNumber = 0;
+
+// rl.on('line', async (line) => {
+//   try {
+//     await traducir(lineNumber, line);
+//     lineNumber++;
+//   } catch (err) {
+//     console.log(`Llegue hasta: Line ${lineNumber}: ${line}`);
+//   }
+// });
+
+const translationsMap = new Map(); // Map to store translations with line numbers
+lineNumber = 1;
 
 rl.on('line', async (line) => {
   try {
-    await traducir(line);
+    const translation = await translate(line, { to: 'es' });
+    const formattedTranslation = `${lineNumber} ${translation.text}`;
+    console.log(formattedTranslation);
+    translationsMap.set(lineNumber, formattedTranslation);
     lineNumber++;
   } catch (err) {
-    console.log(`Llegue hasta: Line ${lineNumber}: ${line}`);
+    console.log(`Error translating line ${lineNumber}: ${line}`);
+    const sortedTranslations = Array.from(translationsMap.values()).sort((a, b) => {
+      const lineNumA = parseInt(a.split(' ')[0]);
+      const lineNumB = parseInt(b.split(' ')[0]);
+      return lineNumA - lineNumB;
+    });
+    
+    const outputContent = sortedTranslations.join('\n');
+    fs.appendFileSync(filePathOutput, outputContent);
+    console.log('Translations completed and written to file.');
   }
 });
 
-// try {
-//   data = fs.readFileSync(filePathSource, 'utf8');
-//   console.log(data);
-// } catch (err) {
-//   console.error(err);
-// }
-
+rl.on('close', () => {
+  const sortedTranslations = Array.from(translationsMap.values()).sort((a, b) => {
+    const lineNumA = parseInt(a.split(' ')[0]);
+    const lineNumB = parseInt(b.split(' ')[0]);
+    return lineNumA - lineNumB;
+  });
+  
+  const outputContent = sortedTranslations.join('\n');
+  fs.writeFileSync(filePathOutput, outputContent);
+  console.log('Translations completed and written to file.');
+});
